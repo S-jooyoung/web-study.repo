013e5c701ce93ba1072b703070f56a5fd6b39420
S-jooyoung <jooyoung.dev@gmail.com>
2021-10-21 20:00:00 +0000

    feat:implement css,js related main,login,membership page

---
+++ project-01/js/set.js
@@ -0,0 +1,17 @@
+<script src="https://artbyus.co.kr/js/jquery-1.12.4.min.js"></script>
+
+<script>
+  $(document).ready(function () {
+    $(".ag-btn01").click(function () {
+      $("#layer").addClass("active");
+    });
+  });
+</script>
+
+<script>
+  $(document).ready(function () {
+    $(".quit-btn").click(function () {
+      $("#layer").removeClass("active");
+    });
+  });
+</script>
