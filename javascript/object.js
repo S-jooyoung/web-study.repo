0141bca6c473eb9b1abe33d811beaa54c8907dc8
S-jooyoung <jooyoung.dev@gmail.com>
2021-12-26 22:44:00 +0900

    feat:JavaScript 오브젝트 사용법정리[2]

--- javascript/object.js
+++ javascript/object.js
@@ -1,4 +1,5 @@
-// Objects
+
+// Objects 
 // one of the JavaScript's data types.
 // a collection of related data and/or functionality.
 // Nearly all objects in JavaScript are instances of Object
@@ -40,3 +41,67 @@ console.log(jooyoung.name);
 console.log(jooyoung['name']);
 jooyoung['hasJob'] = true;
 console.log(jooyoung.hasJob);
+
+// 3. Property value shorthand
+const person1 = {name: 'bob' , age:2 };
+const person2 = {name: 'steve', age:3};
+const person3 = {name: 'dave', age:4};
+const person4 = new Person('jooyoung', 30);
+console.log(person4);
+
+// 4. Constructor function
+function Person(name,age){
+    // this = {};
+    this.name = name;
+    this.age = age;
+    // return this;
+}
+
+// 5. in operator: property existence check (key in obj)
+console.log('name' in jooyoung);
+console.log('age' in jooyoung);
+
+// 6. for..in vs for..of
+// for (key in obj)
+
+for(key in jooyoung){
+    console.log(key);
+}
+
+//for (value of iteralbe)
+const array = [1,2,3,4,5]
+
+for(let i = 0; i < array.length; i++){
+    console.log(array[i]);
+}
+
+for(value of array){
+    console.log(value);
+}
+
+
+
+// 7. Fun cloning
+// object.assign(dest, [obj1 ,obj2, obj3...])
+const user = {name : 'jooyoung' , age:'20'};
+const user2 = user;
+console.log(user);
+
+//old way
+const user3 = {}
+for(key in user){
+    user3[key] = user[key];
+}
+
+console.log(user3);
+
+//new way
+const user4 = Object.assign({}, user);
+console.log(user4);
+
+//another example
+const fruit1 = {color : 'red'};
+const fruit2 = {color : 'blue', size: 'big'}
+const mixed = Object.assign({}, fruit1, fruit2);
+console.log(mixed.color);
+console.log(mixed.size);
