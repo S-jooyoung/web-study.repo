5406289c858d97c46996c3edf2bca11459d46f56
S-jooyoung <jooyoung.dev@gmail.com>
2021-12-31 11:11:36 +0900

    Feat:1차 자바스크립트 완료

--- javascript/async/async.js
+++ javascript/async/async.js
@@ -1,52 +1,50 @@
-'use strict'
+"use strict";
 // async & await
 // clear style of using promise :)
 
 // 1.async
-async function fetchUser(){
-    
-    return 'ellie';
+async function fetchUser() {
+  return "ellie";
 }
-
 
 const user = fetchUser();
 user.then(console.log);
 console.log(user);
 
 // 2. awit
-function delay(ms){
-    return new Promise((resolve)=> setTimeout(resolve, ms));
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function getApple(){
-    await delay(2000);
-    return '🍎';
+async function getApple() {
+  await delay(2000);
+  return "🍎";
 }
 
-async function getBanana(){
-    await delay(1000);
-    return '🍌';
+async function getBanana() {
+  await delay(1000);
+  return "🍌";
 }
 
-async function pickFruits(){
-    const apple = await getApple();
-    const banana = await getBanana();
-    return `${apple}+ ${banana}`;
+async function pickFruits() {
+  const apple = await getApple();
+  const banana = await getBanana();
+  return `${apple}+ ${banana}`;
 }
 
 pickFruits().then(console.log);
 
 // 3. useful Promise APIS
-function pickAllFruits(){
-    return Promise.all([getApple(),getBanana()])
-        .then(fruits => fruits.join(' + '));
-    
+function pickAllFruits() {
+  return Promise.all([getApple(), getBanana()]).then((fruits) =>
+    fruits.join(" + ")
+  );
 }
 
 pickAllFruits().then(console.log);
 
-function pickOnlyOne(){
-    return Promise.race([getApple(),getBanana()]);
+function pickOnlyOne() {
+  return Promise.race([getApple(), getBanana()]);
 }
 
 pickOnlyOne().then(console.log);
