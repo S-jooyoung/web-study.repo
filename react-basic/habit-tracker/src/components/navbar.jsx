cdb044b7e79f6ff99fc216a3cba880312e740095
S-jooyoung <jooyoung.dev@gmail.com>
2022-04-09 20:00:00 +0000

    Refactor: convert package.json && css in habit-tracker

--- react-basic/habit-tracker/src/components/navbar.jsx
+++ react-basic/habit-tracker/src/components/navbar.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { PureComponent } from 'react';
 
-class Navbar extends Component {
+class Navbar extends PureComponent {
   render() {
     return (
       <nav className="navbar">
-        <i className="navbar-log fas fa-leaf"></i>
+        <i className="navbar-logo fas fa-leaf"></i>
         <span>Habit Tracker</span>
         <span className="navbar-count">{this.props.totalCount}</span>
       </nav>
