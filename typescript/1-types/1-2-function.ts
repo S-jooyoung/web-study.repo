da29663a7591f50e5ba5a5d37ccfa4f583384908
S-jooyoung <jooyoung.dev@gmail.com>
2022-07-17 20:00:00 +0000

    Feat: Typescript 함수 정리

---
+++ typescript/1-types/1-2-function.ts
@@ -0,0 +1,56 @@
+{
+  // // JavaScript 💩
+  // function jsAdd(num1, num2){
+  //     return num1 + num2;
+  // }
+
+  // // TypeScript ✨
+  // function add(num1 : number, num2 : number): number{
+  //     return num1 + num2;
+  // }
+
+  // // JavaScript 💩
+  // function jsFetchNum(id){
+  //     // code ...
+  //     // code ...
+  //     // code ...
+  //     return new Promise((resolve, reject)=>{
+  //         resolve(100);
+  //     })
+  // }
+
+  // // TypeScript ✨
+  // function FetchNum(id : string): Promise<number>{
+  //     // code ...
+  //     // code ...
+  //     // code ...
+  //     return new Promise((resolve, reject)=>{
+  //         resolve(100);
+  //     })
+  // }
+
+  // JavaScript ✨ => TypeScript
+  // Optional parameter
+  function printName(firstName: string, lastName?: string) {
+    console.log(firstName);
+    console.log(lastName); // undefined
+  }
+  printName("Shin", "jooyoung");
+  printName("Shin");
+  printName("Anna");
+
+  // Default parameter
+  function printMessage(message: string = "default message") {
+    console.log(message);
+  }
+
+  printMessage();
+
+  // Rest parameter
+  function addNumbers(...nums: number[]) {
+    return nums.reduce((pre, cur) => pre + cur);
+  }
+  console.log(addNumbers(1, 2));
+  console.log(addNumbers(1, 2, 3, 4));
+  console.log(addNumbers(1, 2, 3, 4, 5));
+}
