da4c69d14157df3e298456c547dce9048b94cbce
S-jooyoung <jooyoung.dev@gmail.com>
2023-06-13 22:56:55 +0900

    Feat: flutter - undestanding Classes for Custom Widgets

--- flutter_basic/first_app/lib/main.dart
+++ flutter_basic/first_app/lib/main.dart
@@ -4,27 +4,34 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.deepPurple,
-                  Color.fromARGB(255, 101, 92, 117),
-                ]),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello World",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.deepPurple,
+              Color.fromARGB(255, 101, 92, 117),
+            ]),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
 }
