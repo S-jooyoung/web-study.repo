34698e8b16a04749d840ec04f8191a1c84c491a5
S-jooyoung <jooyoung.dev@gmail.com>
2024-04-21 10:10:00 +0900

    Feat: pratice grapql query resolvers

--- tweetql/server.js
+++ tweetql/server.js
@@ -1,5 +1,16 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const Tweets = [
+  {
+    id: 1,
+    text: "Hello World",
+  },
+  {
+    id: 2,
+    text: "Bye World",
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID
@@ -20,7 +31,18 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets: () => {
+      return Tweets;
+    },
+    tweet: (_, { id }) => {
+      return Tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`);
